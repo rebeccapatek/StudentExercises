e4bee94bf587c978d780fdf4fc2a3fa0bbf1efd9
rebeccapatek <rebeccapatek@gmail.com>
2020-03-10 14:55:41 -0500

    Finished Student Exercises 2

--- Instructor.cs
+++ Instructor.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 
-namespace StudentExercises {
-    public class Instructor {
+namespace StudentExercises
+{
+    public class Instructor
+    {
 
         public string FirstName { get; set; }
 
@@ -10,15 +12,17 @@ namespace StudentExercises {
 
         public string SlackHandle { get; set; }
 
-        public string Cohort { get; set; }
+        public Cohort Cohort { get; set; }
 
         public string Specialty { get; set; }
         //a method to assign an exercise to a student
-        public void assignExercise (Exercise exercise, Student student) {
-            student.Exercises.Add (exercise);
+        public void assignExercise(Exercise exercise, Student student)
+        {
+            student.Exercises.Add(exercise);
         }
         //creating a instructor constructor
-        public Instructor (string first, string last, string handle, string cohort, string specialty) {
+        public Instructor(string first, string last, string handle, Cohort cohort, string specialty)
+        {
             FirstName = first;
             LastName = last;
             SlackHandle = handle;
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StudentExercises
 {
@@ -30,13 +31,13 @@ namespace StudentExercises
             Cohort37.addStudent(Rebecca);
             Cohort35.addStudent(Melanie);
             Cohort36.addStudent(Sam);
-            Instructor Adam = new Instructor("Adam", "Sheaffer", "AdamSheaffer", "Cohort 37", "Vars");
-            Instructor Joe = new Instructor("Joe", "Sheppard", "JoeShep", "Cohort 35", "Voiceovers");
-            Instructor Steve = new Instructor("Steve", "Brownlee", "Chortlehort", "Cohort 38", "Jokes");
+            Instructor Adam = new Instructor("Adam", "Sheaffer", "AdamSheaffer", Cohort37, "Vars");
+            Instructor Joe = new Instructor("Joe", "Sheppard", "JoeShep", Cohort35, "Voiceovers");
+            Instructor Steve = new Instructor("Steve", "Brownlee", "Chortlehort", Cohort38, "Jokes");
 
-            Instructor Rose = new Instructor("Rose", "Witkowsky", "RosieWitWit", "Cohort 37", "Jumpsuits");
-            Instructor Brenda = new Instructor("Brenda", "Long", "BrendaLong", "Cohort 37", "CSS");
-            Instructor Jissie = new Instructor("Jissie", "Bobessie", "Jissiejissie", "Cohort 36", "Coolness");
+            Instructor Rose = new Instructor("Rose", "Witkowsky", "RosieWitWit", Cohort37, "Jumpsuits");
+            Instructor Brenda = new Instructor("Brenda", "Long", "BrendaLong", Cohort37, "CSS");
+            Instructor Jissie = new Instructor("Jissie", "Bobessie", "Jissiejissie", Cohort36, "Coolness");
             Cohort37.addInstructor(Adam);
             Cohort37.addInstructor(Brenda);
             Cohort37.addInstructor(Rose);
@@ -46,6 +47,7 @@ namespace StudentExercises
             Steve.assignExercise(Coffee, Rebecca);
             Rose.assignExercise(Nutshell, Eli);
             Joe.assignExercise(Capston, Melanie);
+            Joe.assignExercise(Capston, Coffey);
             foreach (Student student in Cohort37.Students)
             {
                 student.Description();
@@ -72,6 +74,65 @@ namespace StudentExercises
             Exercises.Add(Representatives);
             Exercises.Add(Coffee);
             Exercises.Add(KandyKorner);
+            List<Cohort> Cohorts = new List<Cohort>();
+            Cohorts.Add(Cohort35);
+            Cohorts.Add(Cohort36);
+            Cohorts.Add(Cohort37);
+            Cohorts.Add(Cohort38);
+            List<Instructor> Instructors = new List<Instructor>();
+            Instructors.Add(Adam);
+            Instructors.Add(Brenda);
+            Instructors.Add(Steve);
+            Instructors.Add(Rose);
+            Instructors.Add(Joe);
+            Instructors.Add(Jissie);
+            var JavaExercises = Exercises.Where(Ex => Ex.ExerciseLanguage == "Javascript");
+            Console.WriteLine("Javascript Exercises:");
+            Console.WriteLine("------------------------------");
+            foreach (var Ex in JavaExercises)
+            {
+                Console.WriteLine($"{Ex.ExerciseName}");
+
+            }
+            var StudentsByCohort = Students.Where(stu => stu.Cohort == Cohort37);
+            Console.WriteLine("Students who are in Cohort 37:");
+            foreach (var stu in StudentsByCohort)
+            {
+                Console.WriteLine($"{stu.FirstName}");
+            }
+            Console.WriteLine("----------------------");
+            var InstructorsByCohort = Instructors.Where(inst => inst.Cohort == Cohort37);
+            Console.WriteLine("Instructors who are in Cohort 37:");
+            foreach (var inst in InstructorsByCohort)
+            {
+                Console.WriteLine($"{inst.FirstName}");
+            }
+            Console.WriteLine("----------------------");
+            var SortedStudentsByLastName = Students.OrderBy(stud => stud.LastName);
+            foreach (var stud in SortedStudentsByLastName)
+            {
+                Console.WriteLine($"{stud.LastName}");
+            }
+
+            var StudentsWhoArentWorkingOnAnything = Students.Where(student => { return student.Exercises.Count == 0 || student.Exercises == null; });
+            Console.WriteLine("Students without exercises:");
+
+            foreach (var student in StudentsWhoArentWorkingOnAnything)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+            Console.WriteLine($"________________________");
+            var StudentsOrderedByExereciseCount = Students.OrderByDescending(Student =>
+            {
+                return Student.Exercises.Count();
+            }).FirstOrDefault();
+            Console.WriteLine($"First student with the most exercises {StudentsOrderedByExereciseCount.FirstName} {StudentsOrderedByExereciseCount.LastName}");
+            var groups = Students.GroupBy(Student => Student.Cohort.CohortName);
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"{group.Count()} in {group.Key}");
+
+            }
 
 
         }
